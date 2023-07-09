378dcc6afe8044eb4778320984890159cf99f39f
seunglo <devseung9056@gmail.com>
2023-07-09 22:55:24 +0900

    proxy - 프록시 패턴 에제

---
+++ proxy/src/test/java/hello/proxy/code/CacheProxy.java
@@ -0,0 +1,26 @@
+package hello.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject {
+
+    private Subject subject;
+
+    private String cacheValue;
+
+    public CacheProxy(Subject target) {
+        this.subject = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("프록시 호출");
+
+        if (cacheValue == null) {
+            cacheValue = subject.operation();
+        }
+
+        return cacheValue;
+    }
+}
